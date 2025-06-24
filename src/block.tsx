507af50f9bd4839442b5f6fea71a7cab775f70c0
fabiensabatie <fabiensabatie@users.noreply.github.com>
2025-06-24 19:39:01 +0000

    💫 Code improvements: config, components, styles

--- src/block.tsx
+++ src/block.tsx
@@ -84,7 +84,7 @@ function SkeletonModel({ modelUrl }: { modelUrl: string }) {
   }
 
   return (
-    <group ref={meshRef} position={[0, -0.25, 0]} scale={[0.25, 0.25, 0.25]}>
+    <group ref={meshRef} position={[0, 0, 0]} scale={[0.25, 0.25, 0.25]}>
       <primitive object={scene} />
     </group>
   );
